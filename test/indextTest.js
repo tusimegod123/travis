48fa30a50c8d673ecd8a6b2af23a7d1b7a0fbd5c
tusimegod123 <43960999+tusimegod123@users.noreply.github.com>
2020-07-06 16:41:21 +0300

    Addede Test dir and made somr simple test

---
+++ test/indextTest.js
@@ -0,0 +1,10 @@
+const expect = require("chai").expect;
+
+
+// eslint-disable-next-line no-undef
+describe("test", () => {
+  // eslint-disable-next-line no-undef
+  it("should return a string", () => {
+    expect("ci with travis").to.equal("ci with travis");
+  });
+});
